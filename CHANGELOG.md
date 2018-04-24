c4f12d02d6cd3e0cb1816bd708cf78dfd4ea41fe
Ken Innes IV <primehalo@gmail.com>
2018-04-23 23:14:40 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [1.1.0] - 2018-04-23
+### Added
+- CHANGELOG.md, license.txt, and README.md files
+- subsilver2 support
+
+### Changed
+- Updated for phpBB 3.2
+- Combined the JavaScript into a single HTML file: overall_footer_after.html
+- Use JQuery to simplify the JavaScript
+- Changed the styles subfolder from 'all' to 'prosilver'
+- Changed the wording "Search your topics" to "Show your topics" to keep it consistent with phpBB 3.2's wording
+- Use service containers instead of global variables in the listener.php file
+
+### Removed
+- The three HTML files that previously held the needed JavaScript code: memberlist_view_user_statistics_after.html, navbar_header_quick_links_after.html, overall_footer_content_after.html
+
+## [1.0.0] - 2014-12-15
+- First release for phpBB 3.1, ported from the Prime Links extension for phpBB 3.0
---
+++ event/listener.php
@@ -0,0 +1,100 @@
+<?php
+/**
+*
+* Prime User Topics extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018 Ken F. Innes IV <https://www.absoluteanime.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace primehalo\primeusertopics\event;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+* Event listener
+*/
+class listener implements EventSubscriberInterface
+{
+	/**
+	* Service Containers
+	*/
+	protected $auth;
+	protected $template;
+	protected $user;
+	protected $board_url;
+	protected $root_path;
+
+	/**
+	* Constructor
+	*
+	* @param \phpbb\auth\auth			$auth		Auth object
+	* @param \phpbb\template\template 	$template	Template object
+	* @param \phpbb\user				$user		User object
+	* @param $root_path					$root_path	phpBB root path
+	* @param $phpExt					$phpExt		php file extension
+	*/
+	public function __construct(
+		\phpbb\auth\auth $auth,
+		\phpbb\template\template $template,
+		\phpbb\user $user,
+		$root_path,
+		$phpExt)
+	{
+		$this->auth			= $auth;
+		$this->template		= $template;
+		$this->user			= $user;
+		$this->root_path	= $root_path;
+		$this->php_ext		= $phpExt;
+	}
+
+	/**
+	* Assign functions defined in this class to event listeners in the core
+	*
+	* @static
+	* @access public
+	*/
+	static public function getSubscribedEvents()
+	{
+		return array(
+			'core.memberlist_prepare_profile_data'	=> 'memberlist_prepare_profile_data',
+			'core.page_header_after'				=> 'page_header_after',
+		);
+	}
+
+	/**
+	* Set the user's topic link variable for the profile page.
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function memberlist_prepare_profile_data($event)
+	{
+		if ($this->auth->acl_get('u_search'))
+		{
+			$this->user->add_lang_ext('primehalo/primeusertopics', 'primeusertopics');
+			$data = $event['data'];
+			$user_id = $data['user_id'];
+			$this->template->assign_var('U_SEARCH_USER_TOPICS',	 append_sid("{$this->root_path}search.{$this->php_ext}", "author_id=$user_id&amp;sr=topics&amp;sf=firstpost"));
+		}
+	}
+
+	/**
+	* Set the self topic link variable for the page header (also used for the main page of the UCP).
+	*
+	* @param object $event The event object
+	* @return null
+	* @access public
+	*/
+	public function page_header_after($event)
+	{
+		$user_id = $this->user->data['user_id'];
+		if ($user_id != ANONYMOUS)
+		{
+			$this->user->add_lang_ext('primehalo/primeusertopics', 'primeusertopics');
+			$this->template->assign_var('U_SEARCH_SELF_TOPICS',	 append_sid("{$this->root_path}search.{$this->php_ext}", "author_id=$user_id&amp;sr=topics&amp;sf=firstpost"));
+		}
+	}
+}
